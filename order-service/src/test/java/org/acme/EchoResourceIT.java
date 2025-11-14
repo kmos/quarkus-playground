610130e2ea05d6a0a0d6d057b5ebf90ab5736e9d
kmos <kmos@commonhaus.dev>
2025-11-14 16:20:44 +0100

    first phase

--- order-service/src/test/java/org/acme/EchoResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class EchoResourceIT extends EchoResourceTest {
-    // Execute the same tests but in packaged mode.
-}
